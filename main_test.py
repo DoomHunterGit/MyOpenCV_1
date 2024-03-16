c2f5fb84cc11f20d8c8e8322ab0485210607af25
Родион <rodion_chernega_external@bat.com>
2024-03-16 10:57:04 +0300

    Тестирование

---
+++ main_test.py
@@ -0,0 +1,9 @@
+import cv2
+
+# Load an image
+image = cv2.imread("C:\Users\Родион\Pictures\Костя_2017.jpeg")
+
+# Display the image
+cv2.imshow("OpenCV Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
